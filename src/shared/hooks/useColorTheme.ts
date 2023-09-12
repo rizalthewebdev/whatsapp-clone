41db87620d864c78239e9930b4dbab7009472377
Khoerul Rizal <rzl.trimelty@gmail.com>
2023-09-12 10:50:46 +0700

    setup react native vector icons for ios

--- src/shared/hooks/useColorTheme.ts
+++ src/shared/hooks/useColorTheme.ts
@@ -1,10 +1,15 @@
 import {useColorScheme} from 'react-native';
 import Colors from '../utils/Colors';
+import {useMemo} from 'react';
 
 const useColorTheme = () => {
   const theme = useColorScheme();
 
-  return Colors?.[theme ?? 'light'];
+  const color = useMemo(() => {
+    return Colors?.[theme ?? 'light'];
+  }, [theme]);
+
+  return color;
 };
 
 export default useColorTheme;
